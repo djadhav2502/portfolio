12aba4a4d33b1e899a75e1429eee5ef0e1b81eb5
djadhav2502 <141901938+djadhav2502@users.noreply.github.com>
2023-08-10 11:51:23 +0530

    Add files via upload

---
+++ Portfolio/logic.js
@@ -0,0 +1,15 @@
+function python(){
+document.getElementById("myimg").src="images/pythonprogramming(pp6072021)_97_1669529687.jpg";
+}
+
+function sql(){
+    document.getElementById("myimg").src="images/fundamentalswithsql_154_1672115292.jpg";
+}
+
+function java(){
+    document.getElementById("myimg").src="images/javaessentials_155_1678367588.jpg";
+}
+
+function wd(){
+    document.getElementById("myimg").src="images/webdesigningessential_177_1684993160.jpg";
+}
